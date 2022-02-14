e2d32e3a09824af78d547ff8a14dc5370e0ae4ae
pares-seux <paresseux.992@gmail.com>
2022-02-14 08:39:16 +0400

    Fixed

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const body = document.querySelector("body");
 const advertise = document.querySelector(".adv");
 const booksList = document.querySelectorAll(".book");
-let bookTitle = booksList[4].querySelector("a");
+const bookTitle = booksList[4].querySelector("a");
 const secondBookChapters = booksList[0].querySelectorAll("li");
 const fifthBookChapters = booksList[5].querySelectorAll("li");
 const sixthBookChapters = booksList[2].querySelectorAll("li");
-let newChapter = sixthBookChapters[8].cloneNode(true);
+const newChapter = sixthBookChapters[8].cloneNode(true);
 
-body.style.backgroundImage = 'url("/image/you-dont-know-js.jpg")';
+body.style.backgroundImage = 'url("image/you-dont-know-js.jpg")';
 
 advertise.remove();
 booksList[0].before(booksList[1]);
 booksList[5].after(booksList[2]);
 booksList[4].after(booksList[3]);
 
-bookTitle.remove();
 bookTitle.textContent = bookTitle.textContent.replace("Пропопипы", "Прототипы");
-booksList[4].querySelector("h2").append(bookTitle);
 
 secondBookChapters[10].before(secondBookChapters[2]);
 secondBookChapters[3].after(secondBookChapters[6]);
